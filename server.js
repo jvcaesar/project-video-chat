e7cbb9c823b866667f1fd468925b8f40c495b724
John Caesar <john.v.caesar@gmail.com>
2020-08-08 03:35:37 +0300

    set up dynamic room routes

--- server.js
+++ server.js
@@ -4,5 +4,21 @@ const server        = require('http').Server(app)       // creating a server to 
 // creates a server, based on the express server and passes that to socket.io,
 // so socket.io knows what server we're actually using and how to interact with that
 const io            = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+// set up the express server
+app.set('view engine', 'ejs')                           // how to render the views
+app.use(express.static('public'))                       // all javascript and css files in public folder
+
+// routers
+app.get('/', (req, res) => {
+    // create a new room and redirect the user to that room
+    res.redirect(`/${uuidV4()}`)                        // gives a dynamic url(room)
+})
+
+// dynamic parameter passed into the url
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
 
 server.listen(3000)
